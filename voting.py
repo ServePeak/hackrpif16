9ef1c8a659b98822bd127736a062a24f1b647ab0
Tiara Tan <tt1506@nyu.edu>
2016-11-12 21:34:45 -0500

    Make it more efficient

--- voting.py
+++ voting.py
@@ -3,7 +3,6 @@ def voting(array):
     bordaScores = []
     
     for x in range(0,len(array[0])):
-        num = len(array) - x - 1
         restaurants.append(array[0][x])
         bordaScores.append(0)
 
@@ -11,7 +10,7 @@ def voting(array):
         for y in x: #goes through the order the user inputted
             for z in restaurants: #goes through the order the user inputted
                 if y == z:
-                    bordaScores[restaurants.index(z)] = bordaScores[restaurants.index(z)] + len(array[0]) - x.index(z) - 1
+                    bordaScores[restaurants.index(z)] = bordaScores[restaurants.index(z)] + len(array[0]) - x.index(z)
 
     for x in restaurants:
          print x, bordaScores[restaurants.index(x)]
@@ -25,6 +24,21 @@ def voting(array):
 
     return restaurants[maxIndex]
 
+def vote(array):
+    pupper = {}
+    for x in range(0,len(array)):
+        for y in range(0, len(array[x])):
+            print array[x][y]
+            if array[x][y] in pupper.keys():
+                pupper[array[x][y]] = pupper[array[x][y]] + len(array[y]) - y
+            else:
+                pupper[array[x][y]] = len(array[x]) - y
+    return pupper
+
+
+
 testArray = [['Subways', 'McDonalds', 'Popeyes'],['Subways', 'Popeyes', 'McDonalds'],['Popeyes', 'Subways', 'McDonalds']]
-result = voting(testArray)
-print 'The highest ranked restaurant is', result
+#result = voting(testArray)
+#print 'The highest ranked restaurant is', result
+result = vote(testArray)
+print result
